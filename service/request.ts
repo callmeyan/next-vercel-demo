a75eae1bbe9a6a4373efda568f32b8634fe3a0a6
callmeyan <yaclty2@gmail.com>
2023-02-09 16:17:43 +0800

    again

--- service/request.ts
+++ service/request.ts
@@ -1,10 +1,14 @@
 const dev = process.env.NODE_ENV !== "production";
+const isCI = process.env.CI;
 export const API_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
     : "https://" + process.env.VERCEL_URL;
 
 export async function request<T>(url: string) {
+  if (isCI) {
+    return Promise.resolve({});
+  }
   const r = await fetch(API_URL + url);
   return (await r.json()) as T;
 }
